faba46f67d895498b6987968d1b1697d5d76ac52
Raphael2790 <rssshev@gmail.com>
2020-02-07 05:02:16 -0300

    Criado os botoes de maximize e close, ajustes no css e conteudo javascript

--- Modulo02/script.js
+++ Modulo02/script.js
@@ -1,2 +1,21 @@
-const modalOverlay = document.querySelectorAll('.modal-overlay');
-const card = document.querySelectorAll('card');
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal')
+
+
+for(let card of cards) {
+  card.addEventListener('click', () => {
+    const modalId = card.getAttribute('id')
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('iframe').src = (`https://rocketseat.com.br/${modalId}`)
+  })
+}
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+  modalOverlay.classList.remove('active');
+  modal.classList.remove('maximize')
+});
+
+document.querySelector('.maximize-modal').addEventListener('click', () => {
+  modal.classList.add('maximize')
+})
