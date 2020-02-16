b44d595ef76704880674f1651d2cd6012316a3be
Raphael2790 <rssshev@gmail.com>
2020-02-15 21:21:08 -0300

    Concluido a primeira etapa do projeto Foodfy

--- Modulo02/desafio02/indexscript.js
+++ Modulo02/desafio02/indexscript.js
@@ -1,12 +1,24 @@
 const modalOverlay = document.querySelector('.modal-overlay');
-const itensGrid = document.querySelectorAll('.modal')
+const itensGrid = document.querySelectorAll('.itens')
+const modalContent = document.querySelector('.modal-content');
+const modal = document.querySelector('.modal')
 
-itensGrid.forEach((item) => {
+
+itensGrid.forEach((item ) => {
   item.addEventListener('click', () => {
+    const itemId = item.getAttribute('id');
+    const itemText = item.querySelector('p').innerText
+    const itemSmallText = item.querySelector('small').innerText
+    console.log(itemId)
+  
     modalOverlay.classList.add('active');
-  })
+    modal.querySelector('img').src = `./assets/${itemId}.png`
+    modal.querySelector('p').innerText = itemText;
+    modal.querySelector('small').innerText = itemSmallText;
+    })
 })
 
-document.querySelector('.close-modal').addEventListener('click',() => {
 
+document.querySelector('.close-modal').addEventListener('click',() => {
+  modalOverlay.classList.remove('active');
 })
