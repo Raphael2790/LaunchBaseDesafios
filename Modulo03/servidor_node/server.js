9a394fbdad76cf5965506959d4728b34e02d7e35
Raphael2790 <rssshev@gmail.com>
2020-02-16 05:16:22 -0300

    Iniciando servidor

---
+++ Modulo03/servidor_node/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views',{
+  express: server
+})
+
+server.get('/', (req, res) =>{
+  return res.render('index')
+})
+
+server.get('/conteudo', (req, res) =>{
+  console.log(res.status)
+  return res.render('conteudo')
+})
+
+server.listen(5000, (err) => {
+  if(err) {
+    return `Erro ${err}`
+  }
+  return 'Servidor rodando na porta 5000'
+})
