eb2fb76e5a5b0b83693db40f4e07c24334c678e3
Raphael2790 <rssshev@gmail.com>
2020-02-16 19:41:56 -0300

    Refatorando desafio01 com nunjucks

---
+++ Modulo03/servidor_node/data.js
@@ -0,0 +1,47 @@
+module.exports = {
+ grid : [
+  {
+    id:'starter',
+    number:01,
+    src:'https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg',
+    name:'Rockeseats Starter',
+    modules:'11 módulos',
+    price:'Grátis'
+  },
+  {
+    id:'launchbase',
+    number:02,
+    src:'https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg',
+    name:'Rockeseats LauchBase',
+    modules:'27 módulos',
+    price:'Pago'
+  },
+  {
+    id:'gostack',
+    number:03,
+    src:'https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg',
+    name:'Rockeseats GoStack',
+    modules:'27 módulos',
+    price:'Pago'
+  }
+],
+
+navbar : [
+  {
+    href:'https://discordapp.com/invite/gCRAFhc',
+    title:'Comunidade'
+  },
+  {
+    href:'mailto:oi@rocketseat.com.br',
+    title:'Email'
+  },
+  {
+    href:'tel:+5547992078767',
+    title:'Telefone'
+  },
+  {
+    href:'/conteudo',
+    title:'Conteudo'
+  }
+]
+}
--- Modulo03/servidor_node/server.js
+++ Modulo03/servidor_node/server.js
@@ -4,25 +4,27 @@ const nunjucks = require('nunjucks');
 
 const server = express();
 
+const data = require('./data')
+
 server.use(express.static('public'));
 
 server.set('view engine', 'njk');
 
 nunjucks.configure('views',{
-  express: server
+  express: server,
+  autoescape:false //faz o nunjucks interpretar html
 })
 
 server.get('/', (req, res) =>{
-  return res.render('index')
+  return res.render('index', {navbar:data.navbar})
 })
 
 server.get('/conteudo', (req, res) =>{
-  console.log(res.status)
-  return res.render('conteudo')
+  return res.render('conteudo', {cards:data.grid,navbar:data.navbar})
 })
 
 server.use(function(req, res) {
-  return res.status(404).render("not-found");
+  return res.status(404).render("not-found",{navbar:data.navbar} );
 });
 
 server.listen(5000, (err) => {
