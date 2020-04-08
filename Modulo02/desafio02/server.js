62a48a13e5c83c7c501981586999623f87a88f54
Raphael2790 <rssshev@gmail.com>
2020-04-07 23:14:33 -0300

    criada a pagina de not-found , rota e estilo

--- Modulo02/desafio02/server.js
+++ Modulo02/desafio02/server.js
@@ -15,15 +15,19 @@ nunjucks.configure('views', {
 })
 
 server.get('/', (req, res) => {
-  return res.render('index');
+  return res.render('index', {css:"style.css"});
 })
 
 server.get('/sobre', (req, res) => {
-  return res.render('sobre')
+  return res.render('sobre', {css:"sobrestyle.css"})
 })
 
 server.get('/receitas', (req, res) => {
-  return res.render('receitas')
+  return res.render('receitas',{css:"receitas.css"})
+})
+
+server.use((req, res) => {
+  return res.status(404).render('not-found', {css:"style.css"});
 })
 
 server.listen(5050, (err) => {
