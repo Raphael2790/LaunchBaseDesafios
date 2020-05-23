710301eccd83cd2ad0683d8bcfdaa4ba91c92d42
Raphael2790 <rssshev@gmail.com>
2020-05-23 17:08:46 -0300

    Criando a paginas para editar cadastros

--- ControleAcademia/instructors.js
+++ ControleAcademia/instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json');
-const { age } = require('./utils');
+const { age , date } = require('./utils');
 
 exports.show = (req, res) => {
 
@@ -57,4 +57,25 @@ exports.post = (req, res) => {
     return res.redirect('/instructors')
   }
   )
+}
+
+exports.edit = (req, res) => {
+
+  const { id } = req.params
+
+  
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return instructor.id == id
+  })
+  
+  if(!foundInstructor) {
+    return res.send('Instrutor não encontrado');
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+
+  return res.render('instructors/edit', { instructor })
 }
