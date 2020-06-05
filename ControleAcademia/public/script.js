651af76b979d1f1fea5c2f087f7b3a39fe796537
Raphael2790 <rssshev@gmail.com>
2020-06-05 08:18:19 -0300

    Começando a estrutura do site para membros

--- ControleAcademia/public/script.js
+++ ControleAcademia/public/script.js
@@ -1,21 +1,8 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
-const modal = document.querySelector('.modal')
+const currentPage = window.location.pathname;
+const menuItens = document.querySelectorAll('header .links a')
 
-
-for(let card of cards) {
-  card.addEventListener('click', () => {
-    const modalId = card.getAttribute('id')
-    window.open(`https://rocketseat.com.br/${modalId}`,'_blank');
-  })
+for(item of menuItens) {
+  if(currentPage.includes(item.getAttribute("href"))){
+    item.classList.add("active")
+  }
 }
-
-/*document.querySelector('.close-modal').addEventListener('click', () => {
-  modalOverlay.classList.remove('active');
-  modal.classList.remove('maximize')
-});
-
-document.querySelector('.maximize-modal').addEventListener('click', () => {
-  modal.classList.add('maximize')
-})
-*/
